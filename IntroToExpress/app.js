777e6dc0641695316934c7a40c44fd5915880dd4
EC2 Default User <ec2-user@ip-172-31-89-117.ec2.internal>
2020-04-17 11:04:56 +0000

    npm i -g nodemon

---
+++ IntroToExpress/app.js
@@ -0,0 +1,18 @@
+var express = require("express");
+var app =express();
+
+app.get("/",function(req,res){
+   res.send("This is Aditya Mandlekar getting ready for Full Stack Development challenges on Cloud Platforms");
+});
+
+app.listen(process.env.PORT,process.env.IP,function(){
+    console.log("Server has Started");
+});
+
+app.get("/myloveyfamily",function(req,res){
+    res.send("Madhura Nihal Mahi Aai Baba ketki Mahesh - You are my World");
+});
+
+app.get("/world",function(req,res){
+    res.send("Vasudhaiva Kutumbakam");
+});
