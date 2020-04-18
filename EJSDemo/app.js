947fac1bca1a832fc02ae06ed5d2739c6fc8ca95
EC2 Default User <ec2-user@ip-172-31-89-117.ec2.internal>
2020-04-18 22:38:15 +0000

    Condition and For Loop EJS Demo

--- EJSDemo/app.js
+++ EJSDemo/app.js
@@ -10,6 +10,22 @@ app.get("/fallinlovewith/:thing",function(req,res){
     res.render("thing.ejs",{thingVar:thing});
 });
 
+app.get("/Posts",function(req,res){
+    var posts =[
+        {
+            userName:"AdityaM",surname:"Mandlekar"
+        },
+        {
+            userName:"AdityaM1",surname:"Mandlekar1"
+        },
+        {
+            userName:"AdityaM2",surname:"Mandlekar2"
+        }
+        ]
+    res.render("posts.ejs",{posts:posts});
+
+});
+
 app.listen(process.env.PORT,process.env.IP ,function(){
     console.log("listening on port "+process.env.PORT);
 });
