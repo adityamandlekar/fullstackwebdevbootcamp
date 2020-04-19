f5cfb4cef255bd9a366129dfb0634b01b675a8da
EC2 Default User <ec2-user@ip-172-31-89-117.ec2.internal>
2020-04-19 19:54:02 +0000

    Some Issues with ES6 Libraries. No issue in Syntax

---
+++ ES6SyntaxSample/ES6.js
@@ -0,0 +1,13 @@
+const request=require('request-promise');
+
+rp('https://jsonplaceholder.typicode.com/users/1')
+    .then((body)-->
+{
+    const parsedData=JSON.parse(body);
+    console.log('${parsedData.name}  lives in ${parsedData.address.city}');
+})
+catch((err)-->
+{
+    console.log('Error!',err);
+});
+   
