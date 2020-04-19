18487abfb17b5f1f6d02fab0bb8feace02ae459b
EC2 Default User <ec2-user@ip-172-31-89-117.ec2.internal>
2020-04-19 21:26:52 +0000

    Movie API Search using Express JS and Forms

---
+++ MovieSearchAPI/app.js
@@ -0,0 +1,26 @@
+var express=require('express')
+var app =express()
+var request=require('request');
+app.set("view engine","ejs");
+
+app.get("/",function(req,res){
+    res.render("search");
+});
+app.listen(process.env.PORT,process.env.IP,function(){
+    console.log("Movie APP has started on port " + process.env.PORT);
+});
+
+app.get("/results",function(req,res){
+     var query=req.query.search;
+     var url="http://www.omdbapi.com/?s="+query;
+     //One needs to add API key to url to work . I am removing Code for security Purpose
+     var key="&apikey=2077500b"; 
+     url+=key;
+    request(url,function(error,response,body){
+     
+       var data=JSON.parse(body);
+       //res.send(result);
+     //  res.send(result["Search"][0]["Title"]);
+     res.render("results",{data:data});
+   }) ;
+});
